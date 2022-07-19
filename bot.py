d24650ea166fbe076b24fc19da43eca3c98ce9a8
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-19 11:41:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ client = roblox.Client(email="email@example.com", username="Example", password="
 def on_ready(bot: roblox.User):
      print(f"{bot.username} is online!")
 
-client.login("roblosecurity")
+client.login(roblosecurity="roblosecurity")
